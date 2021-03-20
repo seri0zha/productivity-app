d3fa6eda835e450297a994673f9e0fede2f2c827
seri0zha <ise200017@gmail.com>
2021-03-21 00:25:52 +0300

    add check todo button

---
+++ src/library/common/components/CheckButton/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const CheckButton: React.FC = () => {
+  return <button className={styles.checkButton}/>
+}
+
+export default CheckButton;
