17e2b7666c72b5bb180ffb9fd9c9eecfa197ca05
seri0zha <ise200017@gmail.com>
2021-06-02 12:14:37 +0300

    renamed DropdownList to ProfileButton

--- src/components/Header/ProfileButton/index.tsx
+++ src/components/Header/ProfileButton/index.tsx
@@ -3,16 +3,15 @@ import { useDispatch } from "react-redux";
 import { setLoggedIn } from "../../../store/actions/Login";
 import useOutsideClick from "../../../hooks/useOutsideCallback";
 import styles from "./styles.module.css";
+import { FirebaseContext } from "../../..";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-interface DropdownProps {
-  userName: string,
-}
-
-const DropdownList: React.FC<DropdownProps> = (props) => {
+const ProfileButton: React.FC = () => {
   const dropdownRef = useRef(null);
   const dispatch = useDispatch();
   const [dropdownListIsCollapsed, setDropdownListIsCollapsed] = useState(true);
-
+  const {auth} = React.useContext(FirebaseContext);
+  const [user] = useAuthState(auth);
   const toggleDropdown = () => {
     setDropdownListIsCollapsed(prevstate => !prevstate);
   }
@@ -20,6 +19,11 @@ const DropdownList: React.FC<DropdownProps> = (props) => {
   const clickOutside = () => {
     console.log('outside');
     setDropdownListIsCollapsed(true);
+  }
+
+  const onLogoutButtonClick = async () => {
+    await auth.signOut();
+    dispatch(setLoggedIn(false));
   }
   
   // (ref to element for outside handling, callback for click handle)
@@ -29,12 +33,12 @@ const DropdownList: React.FC<DropdownProps> = (props) => {
     <div className={styles.dropdownWrapper} ref={dropdownRef}>
         <div className={styles.titleWrapper}>
           <button className={styles.dropdownElement} onClick={toggleDropdown}>
-           {props.userName}
+           {user?.displayName}
           </button>
         </div>
         {!dropdownListIsCollapsed ? 
         <div className={styles.dropdownList}>
-          <button className={styles.dropdownElement} onClick={() => dispatch(setLoggedIn(false))}>
+          <button className={styles.dropdownElement} onClick={onLogoutButtonClick}>
             Logout
           </button>
       </div>:<></>}
@@ -42,4 +46,4 @@ const DropdownList: React.FC<DropdownProps> = (props) => {
   )
 }
 
-export default DropdownList;
+export default ProfileButton;
